1e7d56e7e6ef375f36c176af8ca6da8af29a1415
Reji Joseph <rjoseph82@schools.nyc.gov>
2025-09-09 22:38:38 -0400

    Add files via upload

---
+++ JAVA_1-8/Java-4-Discussion-soln/Main.java
@@ -0,0 +1,30 @@
+class Main {
+  
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function runs when the program begins.  
+
+  void init(){
+    // Number of cars
+    int numOfCars = 5;
+
+    // Investment amount in currency
+    double investmentAmount = 15100.75;
+
+    // Person's first name
+    String firstName = "Jack";
+    
+    // Is a game over?
+    boolean gameOver = false;
+    
+    // Store the first initial of a name
+    char firstInit = 'E';
+
+    // Maximum number of customers in a theater (declare as a constant)
+    final int maxNumOfSeats =100;
+    
+  }
+  
+}
---
+++ JAVA_1-8/Java-8-CW-soln/Main.java
@@ -0,0 +1,23 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+
+  void madlib(String noun, String adj, String verb){
+    System.out.println("The "+ adj + " " + noun+" " + verb);
+  }
+
+  double areaOfSquare(double s){
+    return s*s;
+  }
+
+  double areaOfCircle(double radius){
+    return Math.PI*Math.pow(radius,2); 
+  }
+  
+  void init(){
+    // test your function
+   
+  }
+}
