7504ec6e965cbb6fcc226c34bdbecafe4105e0a1
Reji Joseph <rjoseph82@schools.nyc.gov>
2025-09-15 16:09:42 +0000

    did tasks

--- JAVA_1-8/Java-3-Activity/Main.java
+++ JAVA_1-8/Java-3-Activity/Main.java
@@ -7,7 +7,13 @@ class Main {
   // program begins.  
 
   void init(){
-   System.out.println("Hi");
+    Print your full name
+    System.out.println("R Joseph");
+    Print your full name with your full address
+    Print your full name with your full address using only one print statement (challenge).
+    System.out.println("Joe Smith\n3443 Corp kennedy St\nBayside\n11361");
+   
+    System.out.println("Hi");
     
   }
 
