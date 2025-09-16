08a0600a77e29bb02bd8f87250005a87dd59467f
Reji Joseph <rjoseph82@schools.nyc.gov>
2025-09-16 15:23:30 +0000

    adrs fix

--- JAVA_1-8/Java-3-Activity/Main.java
+++ JAVA_1-8/Java-3-Activity/Main.java
@@ -3,22 +3,16 @@ class Main {
     (new Main()).init();
   }
 
-  // Init function which runs when the 
-  // program begins.  
-
+  // Init function runs when the program begins
   void init(){
     //Print your full name
-    System.out.println("R Joseph");
+    System.out.println("Adam Smith");
 
     //Print your full name with your full address
-     System.out.println("Hi");
-    
-    //Print your full name with your full address using only one print statement (challenge).
-    System.out.println("Joe Smith\n3443 Corp kennedy St\nBayside\n11361");
-   
-   
-    
-  }
+     System.out.println("Adam Smith");
+     System.out.println("3443 Corporal Kennedy St, Bayside, NY 11361");
 
-  
+    //Print your full name with your full address using only one print statement (challenge).
+    System.out.println("Joe Smith\n3443 Corporal Kennedy St\nBayside\nNY 11361");    
+  }  
 }
